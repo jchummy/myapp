12873fa3b530b938b5067ea8197de9e49b9558da
Julfi Chumber <julfichumber@Julfis-MacBook-Pro.local>
2015-07-07 18:48:28 -0700

    installed node modules

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+// app.get('/', function (req, res) {
+//   res.send('Hello World!');
+// });
 
-app.get("/my_name_is/:name", function (req, res) {
-    res.send( "My name is " + req.params.name );
+// app.get("/my_name_is/:name", function (req, res) {
+//     res.send( "My name is " + req.params.name );
+// });
+
+// app.get("/api/users/", function (req, res) {
+//   res.json("{name: 'tara', age: 24}" + "," + "{name: 'Zoe', age: 28}");
+// });
+
+app.get("/api/books/", function (req, res) {
+ res.json("{title: 'hello'}" + "," + "{title: 'hello'}" + "," + "{title: 'hello'}");
 });
 
 
